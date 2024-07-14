5500d2a7f1fe974baf10151d4068d545a675d1dd
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-07-14 17:42:24 +0200

    refactor: extract method IsAnyParameterNull in production code.

--- LongParameterList.App/Calculator.cs
+++ LongParameterList.App/Calculator.cs
@@ -6,16 +6,17 @@ namespace LongParameterList.App
     {
         public int? Sum(int? number1 = null, int? number2 = null)
         {
-            if(number1 == null || number2 == null)
+            if (IsAnyParameterNull(number1, number2))
             {
                 return 0;
             }
             return number1.Value + number2.Value;
         }
 
+
         public int? Subtract(int? number1, int? number2)
         {
-            if (number1 == null || number2 == null)
+            if (IsAnyParameterNull(number1, number2))
             {
                 return 0;
             }
@@ -24,7 +25,7 @@ namespace LongParameterList.App
 
         public int? Multiply(int? number1, int? number2)
         {
-            if (number1 == null || number2 == null)
+            if (IsAnyParameterNull(number1, number2))
             {
                 return 0;
             }
@@ -33,11 +34,15 @@ namespace LongParameterList.App
 
         public int? Split(int? number1, int? number2)
         {
-            if (number1 == null || number2 == null)
+            if (IsAnyParameterNull(number1, number2))
             {
                 return 0;
             }
             return number1.Value / number2.Value;
+        }
+        private static bool IsAnyParameterNull(int? number1, int? number2)
+        {
+            return number1 == null || number2 == null;
         }
     }
 }
