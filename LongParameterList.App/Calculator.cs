e8d48ab886516f42d89d55fcdd879e2b9b510cc2
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-07-14 17:40:23 +0200

    refactor: removed unnecessary method.

--- LongParameterList.App/Calculator.cs
+++ LongParameterList.App/Calculator.cs
@@ -15,38 +15,29 @@ namespace LongParameterList.App
 
         public int? Subtract(int? number1, int? number2)
         {
+            if (number1 == null || number2 == null)
+            {
+                return 0;
+            }
             return number1.Value - number2.Value;
         }
 
         public int? Multiply(int? number1, int? number2)
         {
+            if (number1 == null || number2 == null)
+            {
+                return 0;
+            }
             return number1.Value * number2.Value;
         }
 
         public int? Split(int? number1, int? number2)
         {
-            return number1.Value / number2.Value;
-        }
-
-        public int? DoOperation(bool suma, bool resta, bool multiplica, int? givenNUmber1 = null, int? givenNumber2 = null)
-        {
-            if(givenNUmber1 == null || givenNumber2 == null)
+            if (number1 == null || number2 == null)
             {
                 return 0;
             }
-            if (suma)
-            {
-                return givenNUmber1 + givenNumber2;
-            }
-            if (resta)
-            {
-                return givenNUmber1 - givenNumber2;
-            }
-            if (multiplica)
-            {
-                return givenNUmber1 * givenNumber2;
-            }
-            return givenNUmber1 / givenNumber2;
+            return number1.Value / number2.Value;
         }
     }
 }
