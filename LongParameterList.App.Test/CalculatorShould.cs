e94db080ab475dfad62e9590e8600e91b3a472b8
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-07-14 16:52:40 +0200

    test: red DoSubtractSuccessfully

--- LongParameterList.App.Test/CalculatorShould.cs
+++ LongParameterList.App.Test/CalculatorShould.cs
@@ -18,5 +18,17 @@ namespace LongParameterList.App.Test
 
             Assert.IsTrue(result == givenNUmber1 + givenNumber2);
         }
+
+        [Test]
+        public void DoSubtractSuccessfully()
+        {
+            var givenNUmber1 = 25;
+            var givenNumber2 = 20;
+            var calculator = new Calculator();
+
+            var result = calculator.DoOperation(givenNUmber1, givenNumber2);
+
+            Assert.IsTrue(result == givenNUmber1 - givenNumber2);
+        }
     }
 }
