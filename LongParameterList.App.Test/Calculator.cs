4a98414af65f6ed441822e4e34fb220955e063b8
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-07-14 17:12:12 +0200

    feat: implemented GetZeroWhenSecondNumberParameterIsNull

--- LongParameterList.App.Test/Calculator.cs
+++ LongParameterList.App.Test/Calculator.cs
@@ -8,7 +8,7 @@
 
         public int? DoOperation(bool suma, bool resta, bool multiplica, int? givenNUmber1 = null, int? givenNumber2 = null)
         {
-            if(givenNUmber1 == null)
+            if(givenNUmber1 == null || givenNumber2 == null)
             {
                 return 0;
             }
