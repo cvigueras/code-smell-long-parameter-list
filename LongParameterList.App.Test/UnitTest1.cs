02fbeeec610c0b269bfdf0ce5c71ea5077bdb57a
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-07-14 16:41:50 +0200

    chore: init project.

---
+++ LongParameterList.App.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace LongParameterList.App.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
